d3612b092b3826d201d87b4f480612182630c555
Ethiopian_boy <mullerfromethiopia@gmail.com>
2023-01-03 15:03:25 +0530

    rest parameter

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,7 @@
 export default function returnHowManyArguments(...rest) {
-    let count = 0;
-    for (const iter of rest) {
-        count += 1;
-    }
+  let count = 0;
+  for (const iter of rest) {
+    count += 1;
+  }
   return count;
 }
