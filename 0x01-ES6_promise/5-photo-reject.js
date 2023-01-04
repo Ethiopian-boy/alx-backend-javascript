2caa5a055233715dcb65ff62cc75f596f589d1dc
Ethiopian_boy <mullerfromethiopia@gmail.com>
2023-01-04 12:54:02 +0530

    Promise

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 function uploadPhoto(filename) {
-  return promise.reject(new Error(`${fileName} cannot be processed`));
+  return Promise.reject(new Error(`${fileName} cannot be processed`));
 }
 
 export default uploadPhoto;
