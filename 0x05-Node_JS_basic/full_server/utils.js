44522900ae272a34c46c4aff26e89f87e86784f3
Ethiopian-boy <mullerfromethiopia@gmail.com>
2023-03-19 18:58:11 +0000

    Basics

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,34 +1,49 @@
-  
-const fs = require('fs');
+import fs from 'fs';
 
-function readDatabase(path) {
-  const promise = (resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, dataDb) => {
-      if (err) reject(Error('Cannot load the database'));
-      if (dataDb) {
-        const inFields = {}; // {field: {counter: # of students, students: [list of students]}}
-        const data = dataDb.split('\n');
-        console.log(`Number of students: ${data.length - 1}`);
+/**
+ * Reads the data of students in a CSV data file.
+ * @param {String} dataPath The path to the CSV data file.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns {Promise<{
+ *   String: {firstname: String, lastname: String, age: number}[]
+ * }>}
+ */
+const readDatabase = (dataPath) => new Promise((resolve, reject) => {
+  if (!dataPath) {
+    reject(new Error('Cannot load the database'));
+  }
+  if (dataPath) {
+    fs.readFile(dataPath, (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      if (data) {
+        const fileLines = data
+          .toString('utf-8')
+          .trim()
+          .split('\n');
+        const studentGroups = {};
+        const dbFieldNames = fileLines[0].split(',');
+        const studentPropNames = dbFieldNames
+          .slice(0, dbFieldNames.length - 1);
 
-        for (let i = 1; i < data.length; i += 1) {
-          const line = data[i].split(','); // get each word
-          if (inFields[line[3]]) { // line[3] is the name field
-            inFields[line[3]].counter += 1;
-            inFields[line[3]].students.push(` ${line[0]}`); // line[0] is firstname
-          } else {
-            inFields[line[3]] = { counter: 1, students: [`${line[0]}`] };
+        for (const line of fileLines.slice(1)) {
+          const studentRecord = line.split(',');
+          const studentPropValues = studentRecord
+            .slice(0, studentRecord.length - 1);
+          const field = studentRecord[studentRecord.length - 1];
+          if (!Object.keys(studentGroups).includes(field)) {
+            studentGroups[field] = [];
           }
+          const studentEntries = studentPropNames
+            .map((propName, idx) => [propName, studentPropValues[idx]]);
+          studentGroups[field].push(Object.fromEntries(studentEntries));
         }
-        for (const key in inFields) {
-          if (Object.prototype.hasOwnProperty.call(inFields, key)) {
-            console.log(`Number of students in ${key}: ${inFields[key].counter}. List: ${inFields[key].students}`);
-          }
-        }
-        resolve({ inFields, counter: data.length - 1 });
+        resolve(studentGroups);
       }
     });
-  };
-  return new Promise(promise);
-}
+  }
+});
 
+export default readDatabase;
 module.exports = readDatabase;
